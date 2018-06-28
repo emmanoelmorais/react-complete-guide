861eeddb85f6ec0bc731ed53abe5662b83156aca
Thiago Morais <thiago.morais@pingwlt.com>
2018-06-28 18:10:29 +0100

    ...

--- src/containers/App.js
+++ src/containers/App.js
@@ -55,7 +55,8 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <Cockpit
+          appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           togglePersonsHandler={this.togglePersonsHandler} />
